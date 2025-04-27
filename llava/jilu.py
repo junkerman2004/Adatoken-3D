e581917f4c7103b60209284b223d91c6ab10fdef
Kai Zhang2004 <163530136+junkerman2004@users.noreply.github.com>
2025-04-27 21:46:52 +0800

    Add files via upload

---
+++ llava/jilu.py
@@ -0,0 +1,16 @@
+attention_mask=
+pdrop forward=torch.Size([1, 925])
+before torch.Size([1, 925])
+4d_casual_mask   2D--->4D
+last torch.Size([1, 1, 925, 925])
+
+
+labels ([1,925])
+
+
+input embeds=([1,925,4096])
+
+
+
+
+features= torch.Size([1, 925, 4096])
