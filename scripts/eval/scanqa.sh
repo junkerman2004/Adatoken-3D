db95b127cc2fd4ddc5e43dd76586cf9e634489f3
Kai Zhang2004 <163530136+junkerman2004@users.noreply.github.com>
2025-04-27 21:25:33 +0800

    Add files via upload

---
+++ scripts/eval/scanqa.sh
@@ -0,0 +1,6 @@
+#python /home/zk/llava_3D/llava/eval/model_scanqa.py \
+#        --model-path /home/Datasets/zk/LLaVA-3D-7B\
+#        --question-file /home/zk/llava_3D/ScanQA/llava-3d-scanqa_val_question.json \
+#        --answers-file /home/zk/llava_3D/ScanQA/50_25_12_scanqa_pred_val.json
+#
+python /home/zk/llava_3D/llava/eval/scanqa_evaluator.py
---
+++ scripts/eval/sqa3d.sh
@@ -0,0 +1,6 @@
+python /home/zk/llava_3D/llava/eval/model_sqa3d.py\
+        --model-path /home/Datasets/zk/LLaVA-3D-7B \
+        --question-file /home/zk/llava_3D/sqa_3d/balanced/llava-3d-sqa3d_test_question.json \
+        --answers-file /home/zk/llava_3D/sqa_3d/balanced/50_25_12_pdrop_answer_pred.json
+
+#python /home/zk/llava_3D/llava/eval/sqa3d_evaluator.py
